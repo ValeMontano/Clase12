7b0fe55ebed5c2596497d28d2f857ba446ea27d7
Valentina Montaño <>
2024-11-05 16:50:28 -0600

    actions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get(["/", "/:name"], (req, res) => {
   greeting = "<h1>Hello World. I'm Node App</h1>";
